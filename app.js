dd00f9c955a4726164e39e5046c99a3a0c8a9e80
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-07-20 15:10:18 +0300

    update app.js

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ var UIController = (function() {
 
 })();
 
+
 var controller = (function(budgetCtrl, UICtrl) {
 
     var z = budgetCtrl.publicTest(5);
-    
+
     return {
         anotherPublic: function() {
             console.log(z);
