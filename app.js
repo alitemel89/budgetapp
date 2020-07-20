d63b8a5278ac49d0f34b9bac039f8cf6b641392f
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-07-20 17:30:12 +0300

    add DOMstrings object

--- app.js
+++ app.js
@@ -9,16 +9,25 @@ var budgetController = (function() {
 
 //UI CONTROLLER
 var UIController = (function() {
-
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
     return {
         getInput: function() {
 
             return {
-                type: document.querySelector('.add__type').value, // will be either inc or exp
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
 
             };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
         }
     }
 
@@ -27,6 +36,7 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
     var ctrlAddItem = function() {
         // 1. Get the field input data
         var input = UIController.getInput();
@@ -41,7 +51,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         console.log('It works');
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13 ) {
