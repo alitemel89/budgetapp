6c13c83de5e558db7570af3387a8b09e1f5db377
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-07-20 17:18:36 +0300

    get input data

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
 
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return (add(b));
-        }
-    }
 
 })();
 
+
+
+//UI CONTROLLER
 var UIController = (function() {
 
-    //Some code
+    return {
+        getInput: function() {
+
+            return {
+                type: document.querySelector('.add__type').value, // will be either inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+
+            };
+        }
+    }
 
 })();
 
-
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+        var input = UIController.getInput();
+        console.log(input);
+        // 2. Add the item to the budget controller
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // 3. Add the item to UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        console.log('It works');
     }
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13 ) {
+            ctrlAddItem();
+        }
+    })
+
 })(budgetController, UIController);
