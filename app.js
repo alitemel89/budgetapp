7a41070c1102386677ee9b83b648a63f85d6bf37
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-07-23 19:53:23 +0300

    add clearfields function

--- app.js
+++ app.js
@@ -116,6 +116,18 @@ var UIController = (function() {
 
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            var fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = ""
+            });
+        },
+
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -152,9 +164,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 3. Add the item to UI
         UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate the budget
 
-        // 5. Display the budget on the UI
+        // 4. Clear the fields
+        UICtrl.clearFields();
+
+        // 5. Calculate the budget
+
+        // 6. Display the budget on the UI
 
     };
 
