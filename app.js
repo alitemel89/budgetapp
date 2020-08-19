e1b8b33962b87f4a9b8d689b1b6c0e848c0a8007
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-08-19 12:00:41 +0300

    display month and year

--- app.js
+++ app.js
@@ -167,7 +167,8 @@ var UIController = (function() {
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -247,6 +248,18 @@ var UIController = (function() {
 
         getDOMstrings: function() {
             return DOMstrings;
+        },
+
+        displayMonth: function() {
+            var now, year, month, months;
+            
+            var now = new Date()
+
+            months = ['January', 'February' , 'March' , 'April' , 'May' , 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            month = now.getMonth()
+            year = now.getFullYear()
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year
+            
         },
 
         displayPercentages: function(percentages) {
@@ -386,6 +399,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayMonth()
             setupEventListeners();
             UICtrl.displayBudget({
                 budget: 0,
