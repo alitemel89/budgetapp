8f9fa8a1cf07e82004fb61b9c9073848eb7439fc
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-08-19 11:40:04 +0300

    format numbers

--- app.js
+++ app.js
@@ -169,6 +169,25 @@ var UIController = (function() {
         container: '.container',
         expensesPercLabel: '.item__percentage'
     };
+
+    var formatNumber = function(num, type) {
+        var numSplit, int, dec;
+        num = Math.abs(num)
+        num = num.toFixed(2)
+        numSplit = num.split('.')
+
+        int = numSplit[0]
+        
+
+        if (int.length > 3) {
+            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length)  //input 2310 output 2,310
+        }
+
+        dec = numSplit[1]
+        return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec
+
+    };
+
     return {
         getInput: function() {
 
@@ -200,7 +219,7 @@ var UIController = (function() {
 
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
             // Insert HTML to the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -251,10 +270,14 @@ var UIController = (function() {
             });
         },
 
+       
+
         displayBudget: function(obj) {
-            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+            var type;
+            obj.budget > 0 ? type = 'inc' : type = 'exp'
+            document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget,type);
+            document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc')
+            document.querySelector(DOMstrings.expenseLabel).textContent = formatNumber(obj.totalExp, 'exp')
             
 
             if (obj.percentage > 0) {
